59aa8f0e60510ebabbb4af6b9b1573dd21df2c7b
cooper.lutz <cooper.lutz@avanade.com>
2018-03-04 23:06:26 -0600

    Adding the ServiceWatcher scripts

---
+++ powershell-scripts/ServiceWatcher/Process-Service.ps1
@@ -0,0 +1,23 @@
+﻿<# 
+.SYNOPSIS  
+    This runbook is provided to serve as an Azure Automation Watcher Task Action
+ 
+.AUTHOR CooperLutz
+ 
+#> 
+
+param( 
+    $EVENTDATA
+) 
+
+$Cred = Get-AzureRmAutomationCredential -ResourceGroupName "ResourceGroup01" -Name "MyCredential"
+
+Write-Output("Passed in data is " + ($EVENTDATA.EventProperties.Data | ConvertFrom-Json)) 
+
+$ServiceName = ($EVENTDATA.EventProperties.Data | ConvertFrom-Json).ServiceName
+$ComputerName = ($EVENTDATA.EventProperties.Data | ConvertFrom-Json).ComputerName
+
+Write-Output "Restarting $ServiceName on $ComputerName..."
+
+
+Invoke-Command -ComputerName $ComputerName -ScriptBlock {Restart-Service $ServiceName} -Credential $Cred
---
+++ powershell-scripts/ServiceWatcher/Watch-Service.ps1
@@ -0,0 +1,39 @@
+﻿<# 
+.SYNOPSIS  
+    This runbook is provided to monitor the status of a windows service
+ 
+.AUTHOR CooperLutz
+
+.EXAMPLE 
+    .\Watch-Service -ServiceName Spooler 
+ 
+#> 
+
+Param 
+( 
+    [Parameter(Mandatory = $true)] 
+    $ServiceName,
+
+    [Parameter(Mandatory = $true)] 
+    $ComputerName
+ 
+) 
+
+$Cred = Get-AzureRmAutomationCredential -ResourceGroupName "ResourceGroup01" -Name "MyCredential"
+
+$Status = (Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-Service $ServiceName} -Credential $Cred).Status
+
+# Add Logic to check the status of the service is "Running"
+if ($Status -ne "Running") { 
+    #Setup data to send to the Watcher Action
+    $Properties = @{} 
+    $Properties.ServiceName = $ServiceName
+    $Properties.Status = $Status
+    $Properties.ComputerName = $ComputerName
+        
+    $Data = $Properties | ConvertTo-Json 
+        
+    # Invoke the watcher action, passing the data
+    Invoke-AutomationWatcherAction -Message "Process Service..." -Data $Data
+     
+} 
